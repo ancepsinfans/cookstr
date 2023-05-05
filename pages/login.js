9959081f80efa874bb55a268c41b479a913a99cd
ancepsinfans <zachary.r.bullard@gmail.com>
2023-05-05 18:06:07 +0300

    icons bar; likes; bookmarks; navbar fix; pwa; new user setup

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { UserContext } from "@/components/context/UserProvider";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import { generatePrivateKey, getPublicKey } from "nostr-tools";
 
 export default function LoginPage() {
+    const [keys, setKeys] = React.useState({ pub: '', priv: '' })
     const { toggleLogin, privateKey, savePrivateKey } = React.useContext(UserContext);
     const router = useRouter()
 
+    function genKey() {
+        const priv = generatePrivateKey()
+        const pub = getPublicKey(priv)
+        setKeys({ pub, priv })
+    }
 
     return (
         <main>
@@ -16,20 +24,37 @@ export default function LoginPage() {
                     toggleLogin()
                     router.push('/')
                 }}>
-                    <label>
-                        private key hex:{' '}
-                        <input
-                            required
-                            type='password'
-                            value={privateKey}
-                            onChange={(e) => {
-                                savePrivateKey(e.target.value)
-                            }}
-                        />
-                    </label>
-                    <br />
-                    <button type='submit'>Login</button>
+                    <fieldset>
+                        <label>
+                            private key hex:{' '}
+                            <input
+                                required
+                                type='password'
+                                value={privateKey}
+                                onChange={(e) => {
+                                    savePrivateKey(e.target.value)
+                                }}
+                            />
+                        </label>
+                        <br />
+                        <button type='submit'>Login</button>
+                    </fieldset>
                 </form>
+                <fieldset>
+                    <h3>Don't have a key?</h3>
+                    <aside>Generate one here</aside>
+                    <button onClick={genKey}>
+                        <Image height={25} width={25} src={'/images/running-nostr.gif'} />
+                    </button>
+                    {keys.pub !== '' ?
+                        <>
+                            <p>Private key: {keys.priv}</p>
+                            <p>Public key: {keys.pub}</p>
+                            <p>Save your private key somewhere safe, and don't share it with anyone!</p>
+                        </>
+                        : null
+                    }
+                </fieldset>
             </div>
         </main>
     )
