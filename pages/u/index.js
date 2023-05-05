92e95379767806cbb9d5102288c3720f325fab11
ancepsinfans <zachary.r.bullard@gmail.com>
2023-05-05 15:11:09 +0300

    icons bar; likes; bookmarks; navbar fix; pwa

---
+++ pages/u/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function DNE() {
+
+    //     return <p>Loading...</p>;
+    // } else {
+    //     recipes = events.map(obj => {
+    //         const { content, ...rest } = obj;
+    //         return { ...rest, content: JSON.parse(content) };
+    //     });
+
+    // }
+    return (
+        <main >
+
+            <article>
+
+                go back. this doesn't exist
+            </article>
+        </main >
+    )
+}
+
